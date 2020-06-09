2ae51c891e48763af4e5ba0407d99228687f5721
owenabbott <ocabbott@gmail.com>
2020-06-09 10:59:06 -0500

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,13 @@
 // Code SimplerComponent Here
+import React from 'react'
+
+export default function SimplerComponent(props) {
+    return (
+        <div onClick={props.handleClick}>
+            I am just happy
+        </div>
+    )
+}
+
+//The readme shows the creation of a functional component in the form of an anonymous function
+//but it looks like it can also be written like a regular function, so that's nifty
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={console.log("I say I'm happy, but on the inside, I'm actually quite sad.")} />
   </div>,
   document.getElementById('root')
 );
